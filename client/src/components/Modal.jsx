12ce4381727137d2243ebf53651be6672d3e6cfe
Dunna Avinash <56786344+Avinash905@users.noreply.github.com>
2023-02-11 02:27:54 +0530

    modal button updated

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import '../styles/modal.css'
 import { IoMdClose } from 'react-icons/io'
 
-const Modal = ({ children, setModalOpen, handleClick, btnname, id }) => {
+const Modal = ({ children, setModalOpen, handleClick, btnname }) => {
 
     return (
         <div className="modal flex-center">
             <div className="modal__content">
                 <IoMdClose onClick={() => { setModalOpen(false) }} className='close-btn' />
                 {children}
-                <button className="btn" onClick={btnname === 'Leave Group' ? handleClick(id) : handleClick}>{btnname}</button>
+                {btnname && <button className="btn" onClick={handleClick}>{btnname}</button>}
             </div>
         </div>
     )
