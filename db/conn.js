12aafae5cce2441bb6e5db86a69862519b9369fe
Dunna Avinash <56786344+Avinash905@users.noreply.github.com>
2023-02-11 00:59:38 +0530

    console.log removed

--- db/conn.js
+++ db/conn.js
@@ -3,11 +3,7 @@ mongoose.set("strictQuery", false);
 
 const mongodbconn = mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("mongodb connection established");
-  })
-  .catch(() => {
-    console.log("mongodb connection error");
-  });
+  .then(() => {})
+  .catch(() => {});
 
 module.exports = mongodbconn;
